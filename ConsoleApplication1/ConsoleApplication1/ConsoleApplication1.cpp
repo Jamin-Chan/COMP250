193a268235e9217015ef67e54814097731c5e457
Jamin Chan <54374432+Jamin-Chan@users.noreply.github.com>
2022-04-24 20:29:56 -0700

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+//Jamin Chan 1/21/22
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string userName;
+    cout << "Please enter username - ";
+    cin >> userName;
+    cout << "Hello " << userName << endl;
+}
+
